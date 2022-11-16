866c5043101356856b18e54ad8fa395c75c47dfd
Urmo Võsa <urmo.vosa@gmail.com>
2022-11-16 11:03:02 +0200

    Update container

--- build_docker.sh
+++ build_docker.sh
@@ -1,4 +1,4 @@
-sudo docker build -t quay.io/eqtlgen/popassign:v0.5 .
+sudo docker build -t quay.io/eqtlgen/popassign:v0.6 .
 
 ### Push to quay.io
-#sudo docker push quay.io/eqtlgen/popassign:v0.5
+#sudo docker push quay.io/eqtlgen/popassign:v0.6
